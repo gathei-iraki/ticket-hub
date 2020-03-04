6b6cdfbcfe1c414e9b3ebc8944e8f66bb6f83908
khadija <sumeyahassan34@gmail.com>
2020-03-04 11:09:54 +0300

    Add commit in ticket-hub

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#mpesa").submit(function(event) {
+        event.preventDefault();
+    prompt("enter amount: ")
+    prompt("enter paybill number: ")
+    alert("thank for buying your bus ticket here")
+    )}
+    )}
