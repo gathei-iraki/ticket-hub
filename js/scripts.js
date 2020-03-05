c243a4b42948b6580298b93f69d1b231e2f0aac8
khadija <sumeyahassan34@gmail.com>
2020-03-05 17:03:18 +0300

    Add commit changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
-$(document).ready(function(){
-    $("#mpesa").submit(function(event) {
-        event.preventDefault();
-    prompt("enter amount: ")
-    prompt("enter paybill number: ")
-    alert("thank for buying your bus ticket here")
-    )}
-    )}
+function getTotalAmount() {
+  prompt("Enter your name");
+  
+  prompt("enter your business no");
+    prompt("enter your business no");
+    prompt("enter your account no.");
+    prompt("enter your mpesa pin");
+    alert("Thank you for booking with us");
+    alert("will send you your ticket number to your email address. ");
+    alert("");
+  };
